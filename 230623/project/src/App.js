0cbed4d22fb81302e268d99609197ff7274e8a3f
y-r-m <wkddpfla09@naver.com>
2023-06-23 11:45:38 +0900

    230623

---
+++ 230623/project/src/App.js
@@ -0,0 +1,11 @@
+import Practice01 from "./components/01"; //상대 경로
+function App() {
+  return (
+    <div>
+      <h1>this is Friday</h1>
+      <Practice01 />
+    </div>
+  );
+}
+
+export default App;
